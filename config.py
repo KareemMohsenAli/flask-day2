27af6388da389528bdce1966665099ffa93979ba
hp <kareemtall99@gmail.com>
2023-03-11 01:25:28 +0200

    day2

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+## developemt , production
+class Config:
+    SECRET_KEY=os.urandom(32)
+    @staticmethod
+    def init_app():
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG= True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///example.sqlite"
+
+
+class ProductionConfig(Config):
+    DEBUG= False
+    # postgresql:://username:password@localhost:portnumber/dbname
+    SQLALCHEMY_DATABASE_URI = "postgresql://kemo:12345@localhost:5432/project3"
+
+
+
+projectConfig= {
+    'dev': DevelopmentConfig,
+    'prd': ProductionConfig
+}
---
+++ students/views.py
@@ -0,0 +1,35 @@
+#1
+from app.models import Student,db
+#2
+from flask import  render_template, redirect, url_for, request
+
+
+from app.models import Student,db
+from flask import  render_template, redirect, url_for, request
+def students_index():
+    students = Student.get_all_students()
+    return render_template('students/index.html', students=students)
+
+
+def student_info(id):
+    student = Student.query.get_or_404(id)
+    return render_template('students/show.html', student=student)
+
+
+def student_delete(id):
+    student = Student.query.get_or_404(id)
+    res =student.delete_object()
+    if res :
+        return redirect(url_for("students_index"))
+
+def validateInputs(requestdict: dict):
+    errors = {}
+    if not requestdict["name"]:
+        errors["name"] = 'name required'
+
+    ## check email exists before
+    student = Student.query.filter_by(email=requestdict['email']).first()
+    if student:
+        errors["email"] = 'Email already exists'
+
+    return errors
